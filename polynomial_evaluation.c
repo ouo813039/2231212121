d0f83412733fb21628f635b3581d93db3163100d
ouo813039 <100186848+ouo813039@users.noreply.github.com>
2022-05-09 12:02:21 +0800

    Update polynomial_evaluation.c

--- polynomial_evaluation.c
+++ polynomial_evaluation.c
@@ -1,25 +1,3 @@
-/*
-//===============================old=============================//
-#include <stdio.h>
-#include <math.h>
-double val=0,result=0,a,b;
-int numOfItem=0;
-int main() {
-    printf("請輸入浮點數x的值以及多項式含有的項數正整數y︰\n");
-    for(int i = 0 ; i <= numOfItem ; i++)
-    {
-        scanf("%lf %lf",&a,&b);
-        if(i==0)
-        {
-            val = a;
-            numOfItem = b;
-        }
-        else result += (pow(val,b))*a;
-    }
-    printf("%lf\n", result);
-}
-*/
-//===============================new=============================//
 #include <stdio.h>
 double power(double x, int q) {
     if (q == 0) {
